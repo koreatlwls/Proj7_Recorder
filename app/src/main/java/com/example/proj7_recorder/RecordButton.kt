9c2fc511a92ba7ce32ecc8c9d67724001c0ea66d
ksj <koreatlwls@gmail.com>
2021-12-14 15:57:31 +0900

    finished
    finished

--- app/src/main/java/com/example/proj7_recorder/RecordButton.kt
+++ app/src/main/java/com/example/proj7_recorder/RecordButton.kt
@@ -9,21 +9,24 @@ class RecordButton(
     attrs: AttributeSet
 ) : AppCompatImageButton(context, attrs) {
 
-    fun updateIconWithState(state: State){
-        when(state){
-            State.BEFORE_RECORDING->{
+    init {
+        setBackgroundResource(R.drawable.shape_oval_button)
+    }
+
+    fun updateIconWithState(state: State) {
+        when (state) {
+            State.BEFORE_RECORDING -> {
                 setImageResource(R.drawable.ic_record)
             }
-            State.ON_RECORDING->{
+            State.ON_RECORDING -> {
                 setImageResource(R.drawable.ic_stop)
             }
-            State.AFTER_RECORDING->{
+            State.AFTER_RECORDING -> {
                 setImageResource(R.drawable.ic_play)
             }
-            State.ON_PLAYING->{
+            State.ON_PLAYING -> {
                 setImageResource(R.drawable.ic_stop)
             }
         }
     }
-
 }
